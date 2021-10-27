caa977c6b2787bc59596fd680c1023b9d667e972
tuan ngo <ngoquytuan@gmail.com>
2021-10-27 15:44:48 +0700

    Tao cac project cho cac mach khac
    - main
    - giao tiep
    - slave control
    - temp: project goc

---
+++ slave/Inc/main.h
@@ -0,0 +1,100 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2020 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32g4xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define LED_Pin GPIO_PIN_0
+#define LED_GPIO_Port GPIOA
+#define RD485_Pin GPIO_PIN_1
+#define RD485_GPIO_Port GPIOA
+#define SCSn_Pin GPIO_PIN_0
+#define SCSn_GPIO_Port GPIOB
+#define INTn_Pin GPIO_PIN_1
+#define INTn_GPIO_Port GPIOB
+#define INTn_EXTI_IRQn EXTI1_IRQn
+#define RSTn_Pin GPIO_PIN_2
+#define RSTn_GPIO_Port GPIOB
+#define FR_Pin GPIO_PIN_12
+#define FR_GPIO_Port GPIOB
+#define LCD_RS_Pin GPIO_PIN_10
+#define LCD_RS_GPIO_Port GPIOC
+#define LCD_EN_Pin GPIO_PIN_11
+#define LCD_EN_GPIO_Port GPIOC
+#define LCD_RW_Pin GPIO_PIN_12
+#define LCD_RW_GPIO_Port GPIOC
+#define LCD_D4_Pin GPIO_PIN_3
+#define LCD_D4_GPIO_Port GPIOB
+#define LCD_D5_Pin GPIO_PIN_4
+#define LCD_D5_GPIO_Port GPIOB
+#define LCD_D6_Pin GPIO_PIN_5
+#define LCD_D6_GPIO_Port GPIOB
+#define LCD_D7_Pin GPIO_PIN_6
+#define LCD_D7_GPIO_Port GPIOB
+/* USER CODE BEGIN Private defines */
+#define WZN_ERR -1
+
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
